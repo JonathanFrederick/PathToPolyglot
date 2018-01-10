dafaf2b1c458b391a827d6d99813df7d19a49d13
JonathanFrederick <jrfreder@gmail.com>
2018-01-09 22:32:51 -0500

    giving new Vagrantfile

--- test.sh
+++ test.sh
@@ -5,13 +5,19 @@ then
   ansible-playbook publish.yml
 elif [ $environment = "staging" ]
 then
-  if [ -d .vagrant ]
+  if [ -d .vagrant ] && [ -e Vagrantfile ]
   then
     echo "existing vagrant machine found"
     echo ""
     echo ""
   else
     vagrant init ubuntu/trusty64
+    echo -e "VAGRANTFILE_API_VERSION = \"2\" \r
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config| \r
+  config.vm.box = \"ubuntu/trusty64\" \r
+  config.vm.network \"forwarded_port\", guest: 80, host: 8080 \r
+  config.vm.network \"forwarded_port\", guest: 443, host: 8443 \r
+end" > Vagrantfile
   fi
 else
   echo "'$environment' is not a valid environment"
