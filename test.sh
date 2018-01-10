397f1271c34f2d53cd79b719ca169d3bb853aba0
JonathanFrederick <jrfreder@gmail.com>
2018-01-09 22:13:39 -0500

    if staging, check for vagrant

--- test.sh
+++ test.sh
@@ -3,6 +3,16 @@ environment="$1"
 if [ $environment = "deployment" ]
 then
   ansible-playbook publish.yml
+elif [ $environment = "staging" ]
+then
+  if [ -d .vagrant ]
+  then
+    echo "existing vagrant machine found"
+    echo ""
+    echo ""
+  else
+    vagrant init ubuntu/trusty64
+  fi
 else
   echo "'$environment' is not a valid environment"
 fi
